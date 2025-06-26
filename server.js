c09b1e186f70bfb46cafe61b8bdb636e526578d1
Ashley Laisure <ashley.laisure@gmail.com>
2025-06-26 18:23:08 -0400

    changed cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connection.on('connected', () => {
 });
 
 // app.use(cors());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
